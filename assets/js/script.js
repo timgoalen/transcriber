ccec20700a049a8b5c9d134586231822928844b2
Tim Goalen <timgoalen@hotmail.com>
2023-10-17 12:26:57 +0000

    Initial setup

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const controls = document.getElementById("controls");
+const microphone = document.getElementById("microphone-icon");
+const textContainer = document.getElementById("text-container");
+const textArea = document.getElementById("text-area");
