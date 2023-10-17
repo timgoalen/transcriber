2be4d6df12298df01e34dbb140abf3d225146d80
Tim Goalen <timgoalen@hotmail.com>
2023-10-17 14:16:17 +0000

    Add font size controls and microphone colour indicators

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,18 @@ try {
   }
 
 const controls = document.getElementById("controls");
-const microphone = document.getElementById("microphone-icon");
+const microphoneLink = document.getElementById("microphone-link");
+const microphoneIcon = document.getElementById("microphone-icon");
 const textContainer = document.getElementById("text-container");
 const textArea = document.getElementById("text-area");
+const decreaseFontBtn = document.querySelector(".font-decrease");
+const increaseFontBtn = document.querySelector(".font-increase");
 
 
 // Initialize variables
 let recognising = false;
 let recognition = new SpeechRecognition();
+let paragraph = document.createElement('p');
 
 // Set recognition settings
 recognition.continuous = true;
@@ -32,17 +36,26 @@ recognition.onresult = function (event) {
         }
     }
 
-    textArea.innerHTML = finalTranscript + "<p style='color: gray;'>" + interimTranscript + "</p>";
+    // Create a new <p> element for each final transcript
+    if (finalTranscript) {
+        const pElement = document.createElement("p");
+        pElement.textContent = finalTranscript;
+        textArea.appendChild(pElement);
+    }
+
+    // Append interim transcript to the existing content
+    textArea.innerHTML += "<span style='color: gray;'>" + transcript + "</span>";
 };
 
+
 // Click event handler for the microphone button
-microphone.addEventListener("click", function () {
+microphoneIcon.addEventListener("click", function () {
     if (recognising) {
         recognition.stop();
-        microphone.textContent = "Start Recognition";
+        microphoneIcon.style.color = "#15c415";
     } else {
         recognition.start();
-        microphone.textContent = "Stop Recognition";
+        microphoneIcon.style.color = "#e40000";
     }
 
     recognising = !recognising;
@@ -52,3 +65,21 @@ microphone.addEventListener("click", function () {
 recognition.onerror = function (event) {
     console.error("Speech recognition error: " + event.error);
 };
+
+// Initial font size
+let fontSize = 3; // You can set your desired initial font size here
+
+// Function to increase font size
+function increaseFontSize() {
+    fontSize += 1; // Increase the font size by 2 pixels (you can adjust this value)
+    textArea.style.fontSize = fontSize + "rem";
+}
+
+function decreaseFontSize() {
+    fontSize -= 1; // decrease the font size by 2 pixels (you can adjust this value)
+    textArea.style.fontSize = fontSize + "rem";
+}
+
+decreaseFontBtn.addEventListener("click", decreaseFontSize);
+
+increaseFontBtn.addEventListener("click", increaseFontSize);
