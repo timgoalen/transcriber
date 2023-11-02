e8e715c72ef42aa84c052dc97e2df622616cfcea
Tim Goalen <timgoalen@hotmail.com>
2023-11-02 14:24:21 +0000

    Add delete note functionality

--- assets/js/list-page.js
+++ assets/js/list-page.js
@@ -7,15 +7,43 @@ function addNoteToListView(id, note) {
             <p>${note}</p>
         </div>
         <div class="item-tools">
-            <i class="fa-regular fa-trash-can" id="${id}"></i>
+            <i class="fa-regular fa-trash-can" data-id="${id}"></i>
         </div>
     </div>
     `
     listPageContainer.innerHTML += noteHtml;
 }
 
-for (let i = 0; i < localStorage.length; i++) {
-    const id = localStorage.key(i);
-    const note = localStorage.getItem(id);
-    addNoteToListView(id, note);
+function displaySavedNotes() {
+    if (localStorage.length > 0) {
+        for (let i = 0; i < localStorage.length; i++) {
+            const id = localStorage.key(i);
+            const note = localStorage.getItem(id);
+            addNoteToListView(id, note);
+        }
+    } else {
+        const noNotesHtml = `
+        <div class="list-page-item">
+            <div class="item-text">
+                <p>Create some notes to see them here!</p>
+            </div>
+        </div>
+        `
+        listPageContainer.innerHTML += noNotesHtml;
+    }
 }
+
+displaySavedNotes();
+
+function handleDeleteBtnClick() {
+    id = this.dataset.id.toString();
+    localStorage.removeItem(id);
+    window.location.reload();
+}
+
+const deleteBtns = Array.from(document.getElementsByClassName("fa-trash-can"));
+
+for (btn of deleteBtns) {
+    btn.addEventListener("click", handleDeleteBtnClick)
+}
+
