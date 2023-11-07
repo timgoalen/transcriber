ba6b9f467d5d7dac342f5c3c12b5253e2fdc4543
Tim Goalen <timgoalen@hotmail.com>
2023-11-07 09:53:13 +0000

    Refactor list-page.js

--- assets/js/list-page.js
+++ assets/js/list-page.js
@@ -1,14 +1,13 @@
 const listPageContainer = document.querySelector(".list-page-main");
 
-
+// Get an array of the note keys (IDs)
 const timestampIDs = Object.keys(localStorage);
 // Sort the array into reverse chronological order
 timestampIDs.sort((a, b) => {
     return parseInt(b) - parseInt(a);
 });
 
-// TODO: try without <p> for multi line elipses??..
-function addNoteToListView(id, note) {
+function createNoteHtml(id, note) {
     const noteHtml = `
     <div class="list-page-item" data-id="${id}">
         <div class="item-text">
@@ -22,11 +21,11 @@ function addNoteToListView(id, note) {
     listPageContainer.innerHTML += noteHtml;
 }
 
-function displaySavedNotes() {
+function displayNotesHtml() {
     if (timestampIDs.length > 0) {
         timestampIDs.forEach(id => {
             const note = localStorage.getItem(id);
-            addNoteToListView(id, note);
+            createNoteHtml(id, note);
         });
     } else {
         const noNotesHtml = `
@@ -40,27 +39,30 @@ function displaySavedNotes() {
     }
 }
 
-// Display the notes before getting the btn html elements
-displaySavedNotes();
+// Display the notes before getting the detail view elements
+displayNotesHtml();
 
-// **these need to be here, could combine into some sort of Callback later...
+// Now get the newly rendered detail view elements
 const listPageItems = Array.from(document.getElementsByClassName("list-page-item"));
-const editNoteModal = document.getElementById("edit-note-modal-container");
-const detailViewModalContent = document.getElementById("edit-note-modal-content");
-const editNoteModalText = document.getElementById("edit-note-modal-text");
+const detailViewModalContainer = document.getElementById("detail-view-modal-container");
+const detailViewModalContent = document.getElementById("detail-view-modal-content");
 
-function showNoteDetailModal() {
-    editNoteModal.style.display = "grid";
+for (const item of listPageItems) {
+    item.addEventListener("click", handleItemClick)
+}
+
+function showDetailViewModal() {
+    detailViewModalContainer.style.display = "grid";
 }
 
 function handleItemClick() {
     id = this.dataset.id.toString();
     const note = localStorage.getItem(id);
     const detailViewHtml = `
-        <div id="edit-note-modal-text">
+        <div id="detail-view-modal-text">
             ${note}
         </div>
-        <div id="edit-note-modal-tools-container">
+        <div id="detail-view-modal-tools-container">
             <div id="back-btn-modal">
                 <i class="fa-solid fa-arrow-left"></i>
                 <div>Back</div>
@@ -76,10 +78,15 @@ function handleItemClick() {
         </div>
     `
     detailViewModalContent.innerHTML = detailViewHtml;
-    showNoteDetailModal();
-    // localStorage.removeItem(id);
-    // window.location.reload();
+    showDetailViewModal();
     getDetailViewToolElements()
+}
+
+function handleDetailViewEditBtn() {
+    const id = this.dataset.id;
+    const dataForEditPage = id;
+    const url = "edit-page.html?data=" + encodeURIComponent(dataForEditPage);
+    window.location.href = url;
 }
 
 function handleDetailViewDeleteBtn() {
@@ -88,39 +95,14 @@ function handleDetailViewDeleteBtn() {
     window.location.reload();
 }
 
-function handleDetailViewEditBtn() {
-    const id = this.dataset.id;
-    // var passIdData = id;
-    // var editPageUrl = "edit-page.html?data=" + encodeURIComponent(passIdData);
-    // window.location.href = editPageUrl;
-    // Your JavaScript code on page1.html
-    var noteIdToPass = id;
-    var url = "edit-page.html?data=" + encodeURIComponent(noteIdToPass);
-    window.location.href = url;
-}
-
 function getDetailViewToolElements() {
     const backBtn = document.getElementById("back-btn-modal");
     const editBtn = document.getElementById("edit-btn-modal");
     const deleteBtn = document.getElementById("delete-btn-modal");
 
     backBtn.addEventListener("click", () => {
-        editNoteModal.style.display = "none";
+        detailViewModalContainer.style.display = "none";
     })
     editBtn.addEventListener("click", handleDetailViewEditBtn)
     deleteBtn.addEventListener("click", handleDetailViewDeleteBtn)
-}
-
-// id = this.dataset.id.toString();
-
-
-
-// const deleteBtns = Array.from(document.getElementsByClassName("fa-trash-can"));
-
-// for (const btn of deleteBtns) {
-//     btn.addEventListener("click", handleDeleteBtnClick)
-// }
-
-for (const item of listPageItems) {
-    item.addEventListener("click", handleItemClick)
 }
